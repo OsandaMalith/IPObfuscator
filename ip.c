0e273aae5dd73d4d165457fa72a40f769754d810
Osanda Malith Jayathissa <osandajayathissa@gmail.com>
2016-04-30 16:36:08 -0700

    Create ip.c

---
+++ ip.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void banner() {
+  fflush(stdin);
+  const static char *banner = 
+"\t\t\t---------------------------------\n"
+"\t\t\t    Welcome to IP Obfuscator     \n"
+"\t\t\t     Coded by @OsandaMalith		\n"
+"\t\t\thttp://osandamalith.wordpress.com\n"
+"\t\t\t---------------------------------\n";
+  for(banner; *banner; ++banner) fprintf(stdout, "%c", *banner);            
+}
+
+int isValidIp(char *str) {
+    int segs = 0;    
+    int chcnt = 0;   
+    int accum = 0;   
+
+    if (!str) return 0;
+
+    while (*str) {
+        if (*str == '.') {
+            if (!chcnt) return 0;
+            if (++segs == 4) return 0;
+            chcnt = accum = 0;
+            str++;
+            continue;
+        }
+        if ((*str < '0') || (*str > '9')) return 0;
+        if ((accum = accum * 10 + *str - '0') > 255) return 0;
+        chcnt++;
+        str++;
+    }
+    if (segs != 3) return 0;
+    if (!chcnt) return 0;
+    return 1;
+}
+
+int main(){
+	banner();
+	int i;
+	unsigned int final;
+	char ip[100], *token;
+	unsigned char dec[3];
+	const char deli[2] = ".";
+
+	printf("\n[+] Enter your IP: ");
+	fgets(ip, sizeof(ip), stdin);
+	strtok(ip, "\n");
+	
+	if(!isValidIp(ip)) {
+		fprintf(stderr,"%s","[!] Enter a valid ip");
+		exit(-1);
+	}
+	token = strtok(ip, deli);
+	
+	for(i=0; token; i++){
+		*(dec+i) = atoi(token);
+		token = strtok(NULL, deli);
+	}
+	
+	final = (*dec << 24)|(*(dec+1) << 16)|(*(dec+2) << 8)|*(dec+3);
+
+	printf("\n[~] Obfuscated IP is http://%u", final);
+}

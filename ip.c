6799b9125e9ae1ab370fb8070b25bc777a9ab687
Osanda Malith Jayathissa <osandajayathissa@gmail.com>
2016-04-30 19:09:51 -0700

    Update ip.c

--- ip.c
+++ ip.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/*
+ * License:
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+ * Author: Osanda Malith Jayathissa 
+ * Website: http://osandamalith.wordpress.com
+ */
 void banner() {
   fflush(stdin);
   const static char *banner = 

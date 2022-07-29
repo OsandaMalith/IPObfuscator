f005262995b7fadcf380083325942be1d1fd139e
Osanda Malith Jayathissa <osandajayathissa@gmail.com>
2022-07-29 12:08:24 +0100

    Update ip.c

--- ip.c
+++ ip.c
@@ -7,7 +7,7 @@
  * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
  * Author: Osanda Malith Jayathissa 
- * Website: http://osandamalith.wordpress.com
+ * Website: http://osandamalith.com
  */
 void banner() {
   fflush(stdin);
